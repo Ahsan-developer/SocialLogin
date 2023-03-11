17cfc014eab173e7ec94e798db8f0d38e769acb7
Ahsan-developer <rizwan-zaheer@sendaride.com>
2023-03-11 04:06:14 -0600

    updated scope in instagram link

--- src/socialLogins.tsx
+++ src/socialLogins.tsx
@@ -98,7 +98,7 @@ export function InstaLogin() {
         client_id={process.env.REACT_APP_INSTAGRAM_APP_ID || ""}
         client_secret={process.env.REACT_APP_INSTAGRAM_APP_SECRET || ""}
         redirect_uri={REDIRECT_URI}
-        scope="basic"
+        scope="user_profile,user_media"
         state="1"
         onLoginStart={onLoginStart}
         onLogoutSuccess={onLogoutSuccess}
